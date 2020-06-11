fddaba40b6beec1c3726a5d38d543a9868491bad
Kuddleman <edl1155@gmail.com>
2020-06-11 15:07:21 -0700

    add basic routes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Browser, Route } from 'react-router-dom'
+import { BrowserRouter , Route } from 'react-router-dom'
 
 import Posts from './components/Posts'
 import Profile from './components/Profile'
 import Home from './components/Home'
 import Forms from './components/Forms'
 
-const App = () => {
-  return (
-    <h1>routing</h1>
-  )
-}
+// const App = () => {
+//   return (
+//     <h1>routing</h1>
+//   )
+// }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={ Home }></Route>
+      <Route path="/posts" component={ Posts }></Route>
+      <Route path="/profile" component={ Profile }></Route>
+      <Route path="/Forms" component={ Forms }></Route>
+    </div>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
